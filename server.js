f557689a1b2c05886f8ae7ed091126037f38f14b
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-11 21:41:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,41 @@ var content={
                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                </p>`
     
+};
+function createTemplate(data)
+{
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+
+var htmlTemplate=`
+<!doctype html>
+<html lang="en">
+    <head>
+               <meta name="viewport" content="width=device-width, initial-scaled=1" />
+        <title>
+           ${title}
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div>
+            <a href="/">Home</a></div>
+            <hr/>
+            <h1>${heading}</h1>
+            <div>${date}</div>
+            <div class="container">
+               ${content}
+              </div>
+            
+        
+    </body>
+</html> 
+`;
 }
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
