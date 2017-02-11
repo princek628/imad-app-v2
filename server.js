b1bd7c60c809096b0a7d2bcf59e3d9fb2b389a6f
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-11 21:44:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content={
+var articleOne={
                title:'Article One | Piyush Raja',
                heading:'Article One',
                date:'February 11, 2016',
@@ -48,6 +48,7 @@ var htmlTemplate=`
     </body>
 </html> 
 `;
+return htmlTemplate;
 }
 
 
@@ -56,7 +57,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
