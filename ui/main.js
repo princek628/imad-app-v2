61dd4fb32f588140b1bc6e78f8ac22f1447b8dc1
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-12 21:29:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,14 @@ var element=document.getElementById('main-text');
 
 element.innerHTML='New Vaue';
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-  img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+  //img.style.marginLeft='100px';
   
 };
