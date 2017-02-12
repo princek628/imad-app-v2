3de64bc72c4d56dfc6865397ab8aaaeb72488ea4
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-12 21:05:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='New Vaue';
+var img=getElementById('madi');
+img.onclick=function(){
+  img.style.marginLeft='100px';
+  
+};
