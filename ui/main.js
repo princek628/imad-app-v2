c15901156ed9d4771f7a9791190867f235659214
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-14 00:14:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,13 +24,14 @@ button.onclick=function()
     //capture the response and store in a variable
     request.onreadystatechange=function()
     {
-        if(request.readyState===XMLHttpRequest.DONE)
+        if(request.readyState===XMLHttpRequest.DONE){
         //take some action.
         if(request.status===200){
            var counter= request.responseText;
            var span=document.getElementById('count');
            span.innerHTML=counter.toString();
            
+        }
         }
         
     };
