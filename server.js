be3c2f6744ec7c50c375f061d5db28c36d378c26
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-14 02:52:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 var names=[];
 app.get('/submit-name/:name', function(req,res){
+    var name=req.params.name;
+    names.push(name);
     res.send(JSON.stringyf(names));
 });
 
