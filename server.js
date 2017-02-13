93efd4b0f7da5ff5b287377c114f8d38c7b41e43
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-14 02:57:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,12 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
+var names=[];
+app.get('/submit-name', function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -95,8 +101,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit-name/:name', function(req,res){
-    var name=req.params.name;
+app.get('/submit-name', function(req,res){
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
