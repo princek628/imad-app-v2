e93ce63d9fc9b71afb586dcc020cea2e40580165
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-14 01:12:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ button.onclick=function()
         }
         
     };
-    request.open('GET','piyushrj100.imad.hasura-app.io/counter',true);
+    request.open('GET','http://piyushrj100.imad.hasura-app.io/counter',true);
     request.send(null);
     
    // counter=counter+1;
