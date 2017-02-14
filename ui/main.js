4e2bf6d8aca464aa680e59c21bbf9b0b127dfd17
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-14 08:48:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,15 +55,7 @@ submit.onclick=function(){
         if(request.status===200){
          //capture a list of names and render it on the list
     var names=request.responseText;
-    names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-        }
-        }
+   
         
     };
     //make the request
