9e510dafacf980e76cfb0b3b103a0b200e42b059
piyushrj100 <piyush1.bksc@gmail.com>
2017-02-27 21:14:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,9 +104,22 @@ app.get('/submit-name', function(req,res){
     names.push(name);
     res.send(JSON.stringify(names));
 });
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function(req,res){
+   // var articleName=req.params.articleName;
+    pool.query("SELECT * FROM article  WHERE title=" + req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(res.rows.length===0){
+                res.status(484).send("Article not found");
+            }else{
+                var articleData=result.rows[0];
+                  res.send(createTemplate(articleData));
+            }
+            
+        }
+    });
+ 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
