0e9b00c40a7b7c0dd7e38161a384087387ba0c67
piyushrj100 <piyush1.bksc@gmail.com>
2017-03-13 00:21:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser= require('body-parser');
+var session= require('express-session');
 
 var config={
     user:'piyushrj100',
@@ -15,6 +16,12 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret :'SomeRandomVAlue',
+    cookie: {maxAge: 1000*60*60*24*30}
+}));
+    
+
 var articles={
  'article-one': {
                title:'Article One | Piyush Raja',
@@ -122,6 +129,7 @@ app.post('/login', function(req,res){
                 var salt = dbString.split('$')[2];
                 var hashedpassword=hash(password, salt);
                 if(hashedpassword===dbString){
+                    req.session.auth = {userId: result.rows[0].id};
                     res.send('Credentials correct!');
                     
                 }else{
@@ -132,6 +140,20 @@ app.post('/login', function(req,res){
         } 
         }
        });
+});
+app.get('/check-login',function(req,res){
+   if(req.session && req.session.auth && req.session.auth.userId){
+       res.send('you are logged in' +req.session.auth.userId.toString());
+       
+       
+   } else{
+       req.send('you are not logged in');
+   }
+});
+app.get('/log-out',function(req,res){
+  delete req.session.auth;
+  res.send('logged out');
+  
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
