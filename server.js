44b173d671aa32a1df5e4c985439908561c6d492
piyushrj100 <piyush1.bksc@gmail.com>
2017-03-12 17:32:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto=require('crypto');
 
 var config={
     user:'piyushrj100',
@@ -80,6 +81,14 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+}
+app.get('/hash/:input',function(req,res){
+    var hashString=hash(req.param.input,'this is some random string');
+    res.send(hashedString);
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
